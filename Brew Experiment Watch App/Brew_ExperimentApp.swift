8212a5c9c14174fcc929a6024e113532c80a326f
pure-js <3690368+pure-js@users.noreply.github.com>
2023-08-10 21:39:09 +0400

    Initial Commit

---
+++ Brew Experiment Watch App/Brew_ExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brew_ExperimentApp.swift
+//  Brew Experiment Watch App
+//
+//  Created by Sea on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Brew_Experiment_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Brew Experiment Watch AppUITests/Brew_Experiment_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Brew_Experiment_Watch_AppUITestsLaunchTests.swift
+//  Brew Experiment Watch AppUITests
+//
+//  Created by Sea on 8/10/23.
+//
+
+import XCTest
+
+final class Brew_Experiment_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
