e7b90cd02a87dc1273bb267406be5ac68669f77d
Arthur Green <curious.dev@icloud.com>
2025-04-16 19:57:51 +0400

    build: bump deployment target

--- Brew Experiment Watch App/Brew_ExperimentApp.swift
+++ Brew Experiment Watch App/Brew_ExperimentApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct Brew_Experiment_Watch_AppApp: App {
+struct Brew_Experiment_Watch_App: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
